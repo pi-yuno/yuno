e60e46e0c407cc4c3d004b67a83cc9647236404e
hakureii <hakurei@asia.com>
2023-12-23 14:59:12 +0530

    save yasho from bee

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from cogs import command, headers
 from cogs.message import _message
 from cogs.message_edit import _message_edit
+from cogs.reaction import _reaction_add
 
 bot = command.bot
 
@@ -14,5 +15,8 @@ async def on_message_edit(before, after):
     await _message_edit(before, after)
     await bot.process_commands(after)
 
+@bot.event
+async def on_reaction_add(reaction, user):
+    await _reaction_add(reaction, user)
 
 bot.run(headers.YUNO)
