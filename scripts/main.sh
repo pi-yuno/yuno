e339bd1675d8cb21dc90595ab6391b8604df016f
hakureii <hakureii@asia.com>
2024-01-09 11:13:58 +0530

    ...

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,18 +7,18 @@ ORIGIN="https://$GH_TOKEN@github.com/pi-yuno/yuno.git"
 # deps
 git config --global user.email "hakurei@asia.com"
 git config --global user.name "hakureii"
-git checkout -b $BRANCH
+git checkout -b $BRANCH || echo "branch exists"
 
 pip install -r requirements.txt
 
 function github_update() {
-    git pull --rebase origin master
     git add --all
     git commit -am "schedule ai"
     git push $ORIGIN HEAD:$BRANCH -f
+    git pull --rebase origin master
 }
 
-while true
+while [ -f TRUE ]
 do
     github_update
     python3 bot.py
