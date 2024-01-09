448e1c063f9d62572fda50a52b747c83882c1ca8
hakureii <hakurei@asia.com>
2024-01-09 10:28:34 +0530

    schedule ai

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,6 +4,11 @@
 BRANCH="master"
 ORIGIN="https://$GH_TOKEN@github.com/pi-yuno/yuno.git"
 
+
+# deps
+git config --global "user.email" "hakurei@asia.com"
+git config --global "user.email" "hakureii"
+
 pip install -r requirements.txt
 
 
