3c8685bae104695c2228c85215ca443c293d66a4
hakureii <hakurei@asia.com>
2024-01-09 08:42:11 +0530

    change test

--- bot.py
+++ bot.py
@@ -24,22 +24,29 @@ async def on_reaction_add(reaction, user):
 @tasks.loop(seconds=5)
 async def update_ai_task():
     try:
-        repo = git.Repo('.')
-        repo.git.add('myai.json')
-        repo.git.commit('-am', 'schedule ai update')
-        repo.git.push(f'https://{os.environ["PIYUNO"]}@github.com/pi-yuno/yuno.git', 'master')
+        os.system("git add myai.json")
+        os.system("git commit -am  scheduleai")
+        os.system(f"git push https://{os.environ["PIYUNO"]}@github.com/pi-yuno/yuno.git master -f")
+        #repo = git.Repo('.')
+        #repo.git.add('myai.json')
+        #repo.git.commit('-am', 'schedule ai update')
+        #repo.git.push(f'https://{os.environ["PIYUNO"]}@github.com/pi-yuno/yuno.git', 'master')
     except Exception as e:
-        print(e)
+        pass
+        #print(e)
     try:
-        repo.git.push(f'https://{os.environ["PIYUNO"]}@github.com/pi-yuno/yuno.git', 'master')
+        pass
+        #repo.git.push(f'https://{os.environ["PIYUNO"]}@github.com/pi-yuno/yuno.git', 'master')
     except:
         pass
 
 @bot.event
 async def on_ready():
-    repo = git.Repo('.')
-    repo.config_writer().set_value('user', 'name', 'yuno').release()
-    repo.config_writer().set_value('user', 'email', 'pi-yuno@github.com').release()
+    os.system("git config --global user.name yuno")
+    os.system("git config --global user.email pi-yuno@github.com")
+    # repo = git.Repo('.')
+    # repo.config_writer().set_value('user', 'name', 'yuno').release()
+    # repo.config_writer().set_value('user', 'email', 'pi-yuno@github.com').release()
     update_ai_task.start()
 
 bot.run(headers.YUNO)
