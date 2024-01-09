cccf68cdae15201e3f4f3991314d2b97a7b0871e
hakureii <hakureii@asia.com>
2024-01-09 10:20:33 +0530

    schedule ai

---
+++ scripts/main.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# env
+BRANCH="master"
+ORIGIN="https://GH_TOKEN@github.com/pi-yuno/yuno.git"
+
+pip install -r requirements.txt
+
+
+
+function github_update() {
+	git pull --rebase true
+	git add --all
+	git commit -am "schedule ai"
+	git push $ORIGIN $BRANCH
+}
+
+github_update
