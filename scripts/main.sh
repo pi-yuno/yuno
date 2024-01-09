c56687f7c3404aa9dd7e0bc3db628fc70c91b818
hakureii <hakureii@asia.com>
2024-01-09 11:01:45 +0530

    ..

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,26 +4,23 @@
 BRANCH="train-ai"
 ORIGIN="https://$GH_TOKEN@github.com/pi-yuno/yuno.git"
 
-
 # deps
 git config --global user.email "hakurei@asia.com"
 git config --global user.name "hakureii"
-git checkout -b train-ai
+git checkout -b $BRANCH
 
 pip install -r requirements.txt
 
-
-
 function github_update() {
-	git pull origin master --rebase false
-	git add --all
-	git commit -am "schedule ai"
-	git push $ORIGIN $BRANCH
+    git pull origin master --rebase false
+    git add --all
+    git commit -am "schedule ai"
+    git push $ORIGIN HEAD:$BRANCH
 }
 
-while [ -f TRUE ]
+while true
 do
-	github_update
-	python3 bot.py
+    github_update
+    python3 bot.py
 done
 
